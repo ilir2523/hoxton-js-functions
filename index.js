795845af290af82e7da5adcbb3e6b0af2d942385
ilir2523 <ilirrukaj25@gmail.com>
2021-11-19 14:45:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+/* WRITE THE CODE FOR THE FOLLOWING FUNCTIONS */
+
+// Input: a (number), b (number)
+// Action: Add both numbers together
+// Output: The result (number)
+function add(a, b) {
+    return a + b
+  }
+  
+  // Input: number
+  // Action: Check if the number given is even or not
+  // Output: The result (boolean)
+  function isEven(num) {
+    if (num % 2 == 0) return true //"even"
+    return false //"odd"
+  }
+  
+  // Input: name (string)
+  // Action: Greet the user by name
+  // Output: The greeting (string)
+  function greet(name) {
+    return `Hi, ${name}`
+  }
+  
+  // Input: age (number)
+  // Action: Check if the age is 18 or over
+  // Output: The result (boolean)
+  function isAnAdult(age) {
+    if (age >= 18) return true // 18 or over
+    else return false // under 18
+  }
+  
+  // Input: age (number)
+  // Action: Check how many years are left until adulthood (18)
+  // Output: The result (number)
+  function yearsToAdulthood(age) {
+    /*if (age < 18)*/ 
+    return (18 - age)
+  }
+  
+  // Input: person ({ age: number, name: string })
+  // Action:
+  //   - check if a person is an adult
+  //   - if they are, greet them
+  //   - if they are not, tell them to come back in X years (when they are)
+  // Output: The result (string)
+  function admit(age, name) {
+    // write your code here
+    // use greet, isAnAdult and yearsToAdulthood to help you!
+    if (isAnAdult(age)) return `Hi, ${name}! Come on in.`
+    return `${name} ,go learn JS and come back in ${yearsToAdulthood(age)} years`
+  }
+  
+//   if (age < 18) return `Go learn JS and come back in ${18 - age} year`
+//     else return `Come on in.`
